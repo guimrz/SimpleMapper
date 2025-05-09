b824dd6e39e7d3709e04eb3731505a8c129061a3
José <joseguimaraes96@hotmail.com>
2025-05-09 19:12:35 +0100

    Created the mapping interfaces

---
+++ src/SimpleMapper.Abstractions/IMapper.cs
@@ -0,0 +1,16 @@
+﻿namespace SimpleMapper.Abstractions
+{
+    /// <summary>
+    /// Defines a contract for mapping objects from any source type to a destination type.
+    /// </summary>
+    public interface IMapper
+    {
+        /// <summary>
+        /// Maps an object of any type to a specified destination type.
+        /// </summary>
+        /// <typeparam name="TDestination">The type to map to.</typeparam>
+        /// <param name="source">The source object to map from. It can be of any type.</param>
+        /// <returns>An instance of <typeparamref name="TDestination"/> populated with values from the source.</returns>
+        TDestination Map<TDestination>(object? source);
+    }
+}
---
+++ src/SimpleMapper.Abstractions/ITypeMapper.cs
@@ -0,0 +1,17 @@
+﻿namespace SimpleMapper.Abstractions
+{
+    /// <summary>
+    /// Defines a contract for mapping objects from a source type to a destination type.
+    /// </summary>
+    /// <typeparam name="TSource">The source type to map from.</typeparam>
+    /// <typeparam name="TDestination">The destination type to map to.</typeparam>
+    public interface ITypeMapper<TSource, TDestination>
+    {
+        /// <summary>
+        /// Maps an instance of <typeparamref name="TSource"/> to an instance of <typeparamref name="TDestination"/>.
+        /// </summary>
+        /// <param name="source">The source object to map.</param>
+        /// <returns>A new instance of <typeparamref name="TDestination"/> populated with values from the source.</returns>
+        TDestination Map(TSource source);
+    }
+}
